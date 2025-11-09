a93f64547e027db6f8507217f51c20e5abcefe0f
Ernesto Canquiz <caribestic123@gmail.com>
2025-11-09 12:46:02 -0400

    docs: Update

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,3 +1,4 @@
+import { timestamp } from "drizzle-orm/gel-core";
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core"
 
 export const users = sqliteTable("users", {
@@ -6,4 +7,12 @@ export const users = sqliteTable("users", {
   email: text("email").notNull(),
   login: text("login"),
   password: text("password"), //.notNull(),
+});
+
+export const loginAttempts = sqliteTable("login_attempts", {
+  id: integer("id").primaryKey(),
+  userId: integer("user_id").references(() => users.id),
+  ip: text("ip").notNull(),
+  timestamp: integer("timestamp").notNull(),
+  success: text("success").notNull(),
 });
